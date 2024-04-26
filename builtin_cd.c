9e471fdf3fbaf66cd13ea41c3c720a6819bf6141
Valtteri Kinaret <vkinaret@c2r2p2.hive.fi>
2024-04-26 15:31:19 +0300

    added home and error messages

--- builtin_cd.c
+++ builtin_cd.c
@@ -12,23 +12,35 @@
 
 #include "minishell.h"
 
+static void	update_envp_values(t_shell *ms, char *pwd, char *oldpwd)
+{
+	if (name_exists(ms, "PWD") == 0)
+		envp_update(ms, pwd);
+	if (name_exists(ms, "OLDPWD") == 0)
+		envp_update(ms, oldpwd);
+}
+
 void	builtin_cd(t_shell *ms, char **cmd)
 {
 	char	*pwd;
 	char	*oldpwd;
+	char	*home;
 	char	buf[1000];
 
 	pwd = NULL;
 	oldpwd = NULL;
+	home = envp_exists("HOME", ms);
 	safe_strjoin(&oldpwd, "OLDPWD=", envp_exists("PWD", ms), ms);
-	if (chdir(cmd[1]) == 0)
+	if (!cmd[1] && chdir(home) == 0)
 	{
 		safe_strjoin(&pwd, "PWD=", getcwd(buf, 1000), ms);
-		if (name_exists(ms, "PWD") == 0)
-			envp_update(ms, pwd);
-		if (name_exists(ms, "OLDPWD") == 0)
-			envp_update(ms, oldpwd);
+		update_envp_values(ms, pwd, oldpwd);
+	}
+	else if (chdir(cmd[1]) == 0)
+	{
+		safe_strjoin(&pwd, "PWD=", getcwd(buf, 1000), ms);
+		update_envp_values(ms, pwd, oldpwd);
 	}
 	else
-		error_exit(ERR_FILE, cmd[0], MSG_FILE, ms);
+		error_logger(cmd[0], ": ", MSG_FILE, ms);
 }
