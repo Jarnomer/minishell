e939d0d50573fe83a0d680d2dc498647eeee1e8c
Valtteri Kinaret <vkinaret@c2r2p2.hive.fi>
2024-04-26 17:50:42 +0300

    unset argument parsing added

--- builtin_unset.c
+++ builtin_unset.c
@@ -12,15 +12,36 @@
 
 #include "minishell.h"
 
+static int	error_check(char *str)
+{
+	int i;
+
+	i = 0;
+	if (ft_isdigit(str[i]))
+		return (1);
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_') 
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 void builtin_unset(t_shell *ms, char **cmd, int i, int j)
 {
 	while (cmd[i] != NULL)
 	{
 		j = 0;
-		while (cmd[i][j] != '=' && cmd[i][j] != '\0')
-			j++;
-		if (cmd[i][j] != '=' && name_exists(ms, cmd[i]) == 0)
-			envp_remove(ms, cmd[i]);
+		if (!error_check(cmd[i]))
+		{
+			while (cmd[i][j] != '=' && cmd[i][j] != '\0')
+				j++;
+			if (cmd[i][j] != '=' && name_exists(ms, cmd[i]) == 0)
+				envp_remove(ms, cmd[i]);
+		}
+		else
+			error_logger("export: ", cmd[i], ": not a valid identifier", ms);
 		i++;
 	}
 }
