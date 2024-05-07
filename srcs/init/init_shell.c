adb88fad2a509881b8efe68e5811edec12596587
Valtteri Kinaret <vkinaret@c2r1p2.hive.fi>
2024-05-07 20:16:47 +0300

    hello

--- srcs/init/init_shell.c
+++ srcs/init/init_shell.c
@@ -12,6 +12,20 @@
 
 #include <minishell.h>
 
+static void	increment_shell_level(t_shell *ms)
+{
+	int		shlvl;
+	char	*content;
+	char	*envp;
+
+	envp = NULL;
+	content = envp_exists("SHLVL", ms);
+	shlvl = ft_atoi(content) + 1;
+	content = ft_itoa(shlvl);
+	safe_strjoin(&envp, "SHLVL=", content, ms);
+	envp_update(ms, envp);
+}
+
 static void	create_prompt(t_shell *ms)
 {
 	safe_strdup(&ms->prompt, "", ms);
@@ -43,6 +57,7 @@ void	init_shell(t_shell *ms)
 	ft_bzero(ms, sizeof(*ms));
 	create_prompt(ms);
 	init_envp(ms);
+	increment_shell_level(ms);
 	ms->pipefd[RD_END] = -1;
 	ms->pipefd[WR_END] = -1;
 	ms->tempfd = -1;
